a9b5fcbf8da9c13adb687d5f7118b759f38c040f
anishayadav19 <anisha.yadav10a@gmail.com>
2024-02-14 13:09:32 +0530

    initial project

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <WebView source={{ uri: 'https://www.bujhansi.ac.in/' }} style={{ flex: 1 }} />
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
